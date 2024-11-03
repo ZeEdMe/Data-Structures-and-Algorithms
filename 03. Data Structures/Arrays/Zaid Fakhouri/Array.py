30736d85cd3645b35e03445e9ffcc226a191b8fc
ZeEdMe <22310067@scme.edu.ps>
2024-11-03 10:43:39 +0300

    Create Array.py

---
+++ 03. Data Structures/Arrays/Zaid Fakhouri/Array.py
@@ -0,0 +1,28 @@
+import random
+
+arr1 = [2,3,4,1,32]
+arr2 = [3,6,8,4,9]
+
+print(len(arr1))
+
+print(max(arr1))
+
+print(min(arr1))
+
+print(sum(arr1))
+
+print(arr1)
+
+arr3 = arr1 + arr2
+
+print(arr3)
+
+arr3 *= 2
+
+print(arr3)
+
+print(arr3[0 : 5])
+
+print(2 in arr3)
+
+print(arr3[-1])
