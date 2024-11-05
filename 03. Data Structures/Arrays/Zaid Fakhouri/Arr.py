372d0951ef75319fa3ac6329b91296128cd83a81
ZeEdMe <22310067@scme.edu.ps>
2024-11-05 10:11:27 +0300

    Create Arr.py

---
+++ 03. Data Structures/Arrays/Zaid Fakhouri/Arr.py
@@ -0,0 +1,32 @@
+# arr1 = [2,5,3,32,62,100]
+# i = 0
+
+# while i <= len(arr1):
+#     print(arr1[i])
+#     i += 1
+
+# import math
+
+# arr2 = [Num for Num in range(0,pow(10,6)+1)]
+
+# arr3 = [0.5 * Num for Num in arr2]
+
+# arr4 = [Num for Num in arr3 if Num < 100.5]
+
+# print(arr4)
+
+# arr5 = ["blue","green","red"]
+# arr6 = ["green","blue","red"]
+
+# print(arr5 == arr6)
+
+# print(arr5 != arr6)
+
+# print(arr5 < arr6)
+
+# print(arr6 < arr5)
+
+text = "Hello World".split()
+print(text)
+text2 = "30100501009210045".split("100")
+print(text2)
