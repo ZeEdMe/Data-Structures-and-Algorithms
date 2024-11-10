edcdc3b4dbcdb7a49a3593ac7cf1cf7f260d3db1
ZeEdMe <22310067@scme.edu.ps>
2024-11-10 10:29:32 +0300

    Update Arr.py

--- 03. Data Structures/Arrays/Zaid Fakhouri/Arr.py
+++ 03. Data Structures/Arrays/Zaid Fakhouri/Arr.py
@@ -26,7 +26,31 @@
 
 # print(arr6 < arr5)
 
-text = "Hello World".split()
-print(text)
-text2 = "30100501009210045".split("100")
-print(text2)
+# text = "Hello World".split()
+# print(text)
+# text2 = "30100501009210045".split("100")
+# print(text2)
+
+# def reverse(list):
+#     result = []
+    
+#     for element in list:
+#          result.insert(0,element)
+    
+#     return result
+
+# list1 = [1,2,3,4,5,6]
+# list2 = reverse(list1)
+
+# print(list2)
+# print(list1)
+
+def search(list,key):
+    for x in range(0,len(list)):
+        if key == list[x]:
+            return x
+
+list1 = [1,2,3,4,5,6,7,8,9]
+key = int(input("Enter A Number: "))
+
+print(search(list1,key))
